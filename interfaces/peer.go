386e27dadb16a57fe2ab2bf42fd465c57ed1724a
Rayni Nunez Espino <timetopwm@gmail.com>
2024-01-01 16:20:00 -0400

    Added the new interfaces folder and created two new interfaces: peer and transport.

---
+++ interfaces/peer.go
@@ -0,0 +1,4 @@
+package interfaces
+
+// Peer is an interface that represents the remote node
+type Peer interface{}
---
+++ interfaces/transport.go
@@ -0,0 +1,6 @@
+package interfaces
+
+// Transport is anything that handles the communication
+// between nodes in the network. This can be in the form
+// of (TCP, UDP, Websockets, ...)
+type Transport interface{}
