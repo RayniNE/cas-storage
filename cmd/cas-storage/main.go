064a515a0d6801d5314113b47f2176a176e186fc
Rayni Nunez Espino <timetopwm@gmail.com>
2024-01-01 18:33:14 -0400

    Loaded the .env file and got the LISTEN_ADDRESS env. Also called the ListenAndAccept, and used a select to stop the main threat from quitting.

--- cmd/cas-storage/main.go
+++ cmd/cas-storage/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/RayniNE/cas-storage/peer2peer"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	LISTEN_ADDRESS := os.Getenv("LISTEN_ADDRESS")
+
+	tcpTransport := peer2peer.NewTCPTransport(fmt.Sprintf(":%s", LISTEN_ADDRESS))
+
+	err := tcpTransport.ListenAndAccept()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Application started. Running on PORT: %s", LISTEN_ADDRESS)
+
+	select {}
 }
